cb56c377462faaaae7d443f369f35f9a2581cf16
unknown <aziz.saydaminov02@gmail.com.>
2024-05-08 20:41:34 -0500

    Build 4.

--- Assets/Scripts/Systems/MainMenu.cs
+++ Assets/Scripts/Systems/MainMenu.cs
@@ -1,10 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 using UnityEngine.SceneManagement;
+using System;
 
 public class MainMenu : MonoBehaviour
 {
+    [SerializeField] private TMP_Text txtHighScore;
+
+    private void Start()
+    {
+        txtHighScore.text = "High Score: " + PlayerPrefs.GetInt("High Score").ToString();
+    }
+
     public void StartGame()
     {
         SceneManager.LoadScene(1);
